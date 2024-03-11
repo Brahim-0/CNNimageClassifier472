d16d5ec2ba7d28d2d70f4e5799d8bce42ce8ceca
Brahim <hobrahim01@gmail.com>
2024-03-11 15:33:05 -0400

    added new grid images and file conttaining the image names used in each grid.

--- visulaization_sample_images.py
+++ visulaization_sample_images.py
@@ -12,6 +12,7 @@ emotions = [folder for folder in os.listdir(data_dir) if os.path.isdir(os.path.j
 
 # Function to display a grid of 25 images
 def display_images(emotion):
+    print("---")
     plt.figure(figsize=(10, 10))
     plt.suptitle(emotion, fontsize=16)
     image_files = os.listdir(os.path.join(data_dir, emotion))
@@ -19,6 +20,7 @@ def display_images(emotion):
     for i in range(25):
         plt.subplot(5, 5, i + 1)
         img = mpimg.imread(os.path.join(data_dir, emotion, selected_images[i]))
+        print(selected_images[i])
         plt.imshow(img, cmap='gray')
         plt.axis('off')
     plt.savefig(os.path.join(os.getcwd(), f"{emotion}_grid.png"))  # Save the grid
